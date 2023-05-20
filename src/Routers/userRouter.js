861067d38882a95ed5ef3a1078b2662143c25f4d
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-05-21 02:21:37 +0900

    관리자권한 바꾸는 기능 구현,changeAdmin changeNormal

--- src/Routers/userRouter.js
+++ src/Routers/userRouter.js
@@ -4,12 +4,18 @@ import {
   getUserList,
   getDeleteUser,
   editUser,
+  changeAdmin,
+  changeNormal,
+  getNormalUserList,
 } from "../handler/userHandler";
 const userRouter = express.Router();
 
 userRouter.get("/:id([0-9a-f]{24})", getUser);
 userRouter.delete("/:id([0-9a-f]{24})", getDeleteUser);
 userRouter.get("/", getUserList);
+userRouter.get("/normal", getNormalUserList);
 userRouter.post("/edit", editUser);
+userRouter.get("/chown+/:id([0-9a-f]{24})", changeAdmin);
+userRouter.get("/chown-/:id([0-9a-f]{24})", changeNormal);
 
 export default userRouter;
--- src/handler/userHandler.js
+++ src/handler/userHandler.js
@@ -1,13 +1,45 @@
 import User from "../models/User";
 
 export const getUserList = async (req, res) => {
-  const users = await User.find({});
+  const users = await User.find();
   if (users === null) {
     return res.status(404);
   }
   return res.status(200).json(users);
 };
+export const getNormalUserList = async (req, res) => {
+  const users = await User.find({ adminStatus: false });
+  if (users === null) {
+    return res.status(404);
+  }
+  return res.status(200).json(users);
+};
+export const changeAdmin = async (req, res) => {
+  const _id = req.params.id;
+  if (_id === null) {
+    return res.status(400).json({ message: "no params" });
+  }
+  try {
+    await User.updateMany({ _id }, { $set: { adminStatus: true } });
+    return res.status(204).json({ message: "수정완료!" });
+  } catch (err) {
+    return res.status(500);
+  }
+};
+export const changeNormal = async (req, res) => {
+  const _id = req.params.id;
+  const exist = await User.exists({ _id, adminStatus: false });
 
+  if (exist) {
+    return res.status(400).json({ message: "이미 관리자가 아닙니다" });
+  }
+  try {
+    await User.updateMany({ _id }, { $set: { adminStatus: false } });
+    return res.status(204).json({ message: "관리자가 되었습니다" });
+  } catch (err) {
+    return res.status(500);
+  }
+};
 export const getUser = async (req, res) => {
   const _id = req.params.id;
   if (_id === null) {
